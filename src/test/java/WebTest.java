41457aeca082dc35df403bcbaca9fb493bfc4410
Petr <petr_mameev@mail.ru>
2022-08-22 15:49:49 +0700

    thrid commit

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,27 +1,45 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebTest {
     private WebDriver driver;
+
     @BeforeAll
-    static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    static void setUpAll() {
+//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
+
     @BeforeEach
-    void setUp() {driver = new ChromeDriver();
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
+
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         driver.quit();
         driver = null;
     }
+
     @Test
-    void shouldTest(){
-        
+    void shouldTest() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.name("name")).sendKeys("Мамеев Петр");
+        driver.findElement(By.name("phone")).sendKeys("+79991234567");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        Assertions.assertEquals(expected, actual);
     }
 
 }
